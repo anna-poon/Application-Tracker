fd3987a79425b012fd54f7e40366782a64916ef2
agp5 <anna.poonn@gmail.com>
2023-04-05 16:24:37 -0500

    added views.py

---
+++ app/app/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+import json
+from django.contrib.auth.models import User #####
+from django.http import JsonResponse , HttpResponse ####
+
